3ae3351708702670d93226d6bfc86487b9c8f790
ankit4a4 <singhankit23038@gmail.com>
2025-10-01 15:40:37 +0530

    letest code push

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -4,29 +4,29 @@ import img from "../../../public/images/ourpilocipy.jpg"
 const About = () => {
   return (
     <section id="about" className="bg-[#DDCFCA] py-8 md:py-20 px-6 md:px-10">
-      <div className="container mx-auto grid lg:grid-cols-2 gap-4 md:gap-16 ">
+      <div className="container mx-auto md:flex  gap-4 md:gap-16 ">
 
         {/* Images Left Side */}
-        <div className="flex gap-6 relative ">
-          <div className=" md:w-1/2">
+        <div className="flex gap-6 flex-col  relative md:w-[40%] ">
+          <div className="md:flex md:items-end md:justify-end ">
             <img
               src="/Founder.webp"
               alt="Interior"
-              className="rounded-2xl  md:absolute md:top-0 md:left-0 shadow-2xl md:h-[390px] md:w-[49%] object-cover"
+              className="rounded-2xl mt-3  md:top-0 md:left-0 shadow-2xl md:h-[320px] md:w-[80%] object-cover"
             />
           </div>
-          <div className="md:w-1/2 hidden md:flex md:items-end">
+          <div className=" hidden md:flex md:items-end md:justify-end">
             <img
               src={img}
               alt="Founder"
-              className="rounded-2xl md:absolute md:bottom-0 md:right-0 shadow-2xl md:h-[390px] md:w-[49%] object-cover"
+              className="rounded-2xl  md:bottom-0 md:right-0 shadow-2xl md:h-[300px] md:w-[80%] object-cover"
             />
           </div>
         </div>
 
         {/* Content Right Side */}
-        <div>
-          <p className="uppercase text-sm tracking-widest mb-3 text-[#2D384B]">
+        <div className="md:w-[60%]">
+          <p className="uppercase text-sm tracking-widest mt-3 mb-3 text-[#2D384B]">
             Our Story
           </p>
           <h2 className="text-4xl md:text-5xl font-serif font-bold mb-6 text-[#2D384B]">
@@ -41,7 +41,7 @@ const About = () => {
             alt="Interior"
             className="rounded-2xl mb-3 md:hidden md:absolute md:top-0 md:left-0 shadow-2xl md:h-[390px] md:w-[49%] object-cover"
           />
-          <h2 className="text-4xl md:text-5xl font-serif font-bold mb-6 text-[#2D384B]">
+          <h2 className="text-4xl md:text-5xl font-serif md:mt-[73px] font-bold mb-6 text-[#2D384B]">
             Our Philosophy
           </h2>
           <p className="text-lg leading-relaxed mb-4 text-[#2D384B]/80">
